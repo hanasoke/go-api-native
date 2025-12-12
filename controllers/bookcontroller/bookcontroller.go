47edd24e5f763b13c783d08062487e07fc36c5f9
hanasbp <hanasoke@gmail.com>
2025-12-12 20:49:36 +0700

    updated bookcontroller page

--- controllers/bookcontroller/bookcontroller.go
+++ controllers/bookcontroller/bookcontroller.go
@@ -1,6 +1,7 @@
 package bookcontroller
 
 import (
+	"encoding/json"
 	"go-api-native/config"
 	"go-api-native/helper"
 	"go-api-native/models"
@@ -16,4 +17,14 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helper.Response(w, 200, "List Book's", books)
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	defer r.Body.Close()
 }
