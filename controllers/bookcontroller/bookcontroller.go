1398b7f8e6f55d7585f3c76c33bffc09029fd8c1
hanasbp <hanasoke@gmail.com>
2025-12-13 07:06:43 +0700

    added relationship between book & author table

--- controllers/bookcontroller/bookcontroller.go
+++ controllers/bookcontroller/bookcontroller.go
@@ -14,7 +14,7 @@ import (
 func Index(w http.ResponseWriter, r *http.Request) {
 	var books []models.Book
 
-	if err := config.DB.Find(&books).Error; err != nil {
+	if err := config.DB.Joins("Author").Find(&books).Error; err != nil {
 		helper.Response(w, 500, err.Error(), nil)
 		return
 	}
--- models/books.go
+++ models/books.go
@@ -6,6 +6,7 @@ type Book struct {
 	ID          uint      `gorm:"primaryKey" json:"id"`
 	Title       string    `json:"title"`
 	AuthorID    uint      `json:"author_id"`
+	Author      Author    `gorm:"foreignKey:AuthorID" json:"author"`
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
