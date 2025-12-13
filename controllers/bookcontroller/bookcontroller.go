f8aee1fde216da5fd959004db1f194647d04d21e
hanasbp <hanasoke@gmail.com>
2025-12-13 09:52:04 +0700

    added update bookcontroller

--- controllers/bookcontroller/bookcontroller.go
+++ controllers/bookcontroller/bookcontroller.go
@@ -73,3 +73,42 @@ func Detail(w http.ResponseWriter, r *http.Request) {
 
 	helper.Response(w, 200, "Detail Book", BookResponse)
 }
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	idParams := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParams)
+
+	var book models.Book
+
+	if err := config.DB.First(&book, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helper.Response(w, 404, "Book Not Found", nil)
+			return
+		}
+
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	var bookPayLoad models.Book
+	if err := json.NewDecoder(r.Body).Decode(&bookPayLoad); err != nil {
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	defer r.Body.Close()
+
+	var author models.Author
+	if bookPayLoad.AuthorID != 0 {
+		if err := config.DB.First(&author, bookPayLoad.AuthorID).Error; err != nil {
+
+		}
+	}
+
+	if err := config.DB.Where("id = ?", id).Updates(&author).Error; err != nil {
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	helper.Response(w, 201, "Success Update Author", nil)
+}
--- routes/book.go
+++ routes/book.go
@@ -12,6 +12,6 @@ func BookRoutes(r *mux.Router) {
 	router.HandleFunc("", bookcontroller.Index).Methods("GET")
 	router.HandleFunc("", bookcontroller.Create).Methods("POST")
 	router.HandleFunc("/{id}/detail", bookcontroller.Detail).Methods("GET")
-	// router.HandleFunc("/{id}/update", bookcontroller.Update).Methods("PUT")
+	router.HandleFunc("/{id}/update", bookcontroller.Update).Methods("PUT")
 	// router.HandleFunc("/{id}/delete", bookcontroller.Delete).Methods("DELETE")
 }
