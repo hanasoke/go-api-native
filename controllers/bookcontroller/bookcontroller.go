8c140d6b5f83cd4abccda59ddec1b925728e9b79
hanasbp <hanasoke@gmail.com>
2025-12-13 07:48:47 +0700

    added Detail Controller for Book

--- controllers/bookcontroller/bookcontroller.go
+++ controllers/bookcontroller/bookcontroller.go
@@ -7,7 +7,9 @@ import (
 	"go-api-native/helper"
 	"go-api-native/models"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
@@ -50,4 +52,24 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helper.Response(w, 201, "Success create book", nil)
+}
+
+func Detail(w http.ResponseWriter, r *http.Request) {
+	idParams := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParams)
+
+	var book models.Book
+	var BookResponse models.BookResponse
+
+	if err := config.DB.Joins("Author").First(&book, id).First(&BookResponse).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helper.Response(w, 404, "Book Not Found", nil)
+			return
+		}
+
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	helper.Response(w, 200, "Detail Book", BookResponse)
 }
